25d36542fbe35d5d6065ea7b27cdfb9211784c2b
Nika Nikolishvili <nniko18@freeuni.edu.ge>
2021-01-09 20:54:38 +0400

    Add sidebar on profile button click

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,31 @@
-let logo = document.querySelector(".logo");
-let html = document.querySelector("html");
+let logo = document.querySelector(".logo")
+let filterButton = document.querySelector(".filter_button")
+let profileButton = document.querySelector(".profile_button")
+let html = document.querySelector("html")
+let sidebar = document.querySelector("aside")
 
-initialize();
+initialize()
 
 function initialize() {
     logo.addEventListener("click", scrollToTop)
+    document.addEventListener("click", function (event) {
+        if (sidebar.offsetWidth === 0) {
+            if (event.target === profileButton) {
+                openSidebar(200)
+            }
+        } else if (!sidebar.contains(event.target)) {
+            closeSidebar()
+        }
+    })
 
-    window.onscroll = function() {scrolledTooFarDownCheck(10000)};
+    window.onscroll = function () {
+        scrolledTooFarDownCheck(10000)
+    }
 }
 
 function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
 }
 
 function scrolledTooFarDownCheck(limit) {
@@ -20,4 +34,15 @@ function scrolledTooFarDownCheck(limit) {
     } else {
         html.style.scrollBehavior = "smooth"
     }
+}
+
+
+function openSidebar(width) {
+    sidebar.style.width = width.toString() + "px"
+    html.style.overflow = "hidden"
+}
+
+function closeSidebar() {
+    sidebar.style.width = "0"
+    html.style.overflow = "visible"
 }

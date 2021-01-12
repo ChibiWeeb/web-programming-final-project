b6fc44bc1a40ca01c3ed6e2ad994ff33937d126f
Nika Nikolishvili <nniko18@freeuni.edu.ge>
2021-01-12 21:52:13 +0400

    Add filter menu animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
-let logo = document.querySelector(".logo")
-let filterButton = document.querySelector(".filter_button")
-let profileButton = document.querySelector(".profile_button")
-let html = document.querySelector("html")
-let sidebar = document.querySelector("aside")
-let dimmedOverlay = document.querySelector(".dimmed_overlay")
+const logo = document.querySelector(".logo")
+const filterButton = document.querySelector(".filter_button")
+const profileButton = document.querySelector(".profile_button")
+const html = document.querySelector("html")
+const sidebar = document.querySelector("aside")
+const dimmedOverlay = document.querySelector(".dimmed_overlay")
+const filterMenu = document.querySelector(".filter_menu")
 
 initialize()
 
 function initialize() {
     logo.addEventListener("click", scrollToTop)
-    profileButton.addEventListener("click", function () {openSidebar(200)}) //TODO: maybe fade color in/out?
-    dimmedOverlay.addEventListener("click", closeSidebar)
+    filterButton.addEventListener("click", openFilterMenu)
+    profileButton.addEventListener("click", function () {
+        openSidebar(200)
+    }) //TODO: maybe fade color in/out?
+    dimmedOverlay.addEventListener("click", function () {
+        closeSidebar()
+    })
+
+    filterMenu.addEventListener("click", closeFilterMenu)
 
     window.onscroll = function () {
         scrolledTooFarDownCheck(10000)
@@ -30,7 +38,6 @@ function scrolledTooFarDownCheck(limit) {
     }
 }
 
-
 function openSidebar(width) {
     sidebar.style.width = width.toString() + "px"
     html.style.overflow = "hidden"
@@ -41,4 +48,14 @@ function closeSidebar() {
     sidebar.style.width = "0"
     html.style.overflow = "visible"
     dimmedOverlay.style.display = "none"
+}
+
+function openFilterMenu() {
+    filterButton.src = "images/filter_icon_filled.png"
+    filterMenu.style.top = "100px"
+}
+
+function closeFilterMenu() {
+    filterButton.src = "images/filter_icon.png"
+    filterMenu.style.top = "-500px"
 }

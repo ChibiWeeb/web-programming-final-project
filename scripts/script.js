8af09fdc695de6441c0874cf818a50e95dd8a744
Nika Nikolishvili <nniko18@freeuni.edu.ge>
2021-01-17 22:16:27 +0400

    Add pagination

--- scripts/script.js
+++ scripts/script.js
@@ -14,11 +14,17 @@ const filterApplyButton = document.querySelector(".filter_apply_button")
 
 const hotelListArea = document.querySelector(".hotel_list")
 
+const previousPage = document.querySelector(".previous_page")
+const nextPage = document.querySelector(".next_page")
+
 
 const filterItemValues = new Array(filterItems.length).fill(false)
 
 
 const hotelObjectStoreName = "hotelObjectStore"
+const hotelNumberOnEachPage = 20
+
+let activePageNum = 1
 
 
 databaseInit()
@@ -39,23 +45,32 @@ function databaseInit() {
         let transaction = database.transaction(hotelObjectStoreName, "readwrite")
         let hotelObjectStore = transaction.objectStore(hotelObjectStoreName)
 
-        let hotelObject1 = new Hotel("name1", "https://i.picsum.photos/id/645/300/200.jpg?hmac=zSwhK0ipngA3Q8gPxiRsL3NGUmKtCy89EcH8zONvh8M")
-        let hotelObject2 = new Hotel("name2", "https://i.picsum.photos/id/78/300/200.jpg?hmac=n7Sj8aECCS5WoJkWO-zflsPVDeezs5U4M7WgQjLACA0")
-        let hotelObject3 = new Hotel("name3", "https://i.picsum.photos/id/641/300/200.jpg?hmac=YpOnhDuvo6GeXZPS8yR6Wf62YHrXGlWTDObszZf3zpI")
-        let hotelObject4 = new Hotel("name4", "https://i.picsum.photos/id/331/300/200.jpg?hmac=uIUrcfY5rEjbMirOCMEvEXzC0tIJzFIBfZBZ4qhvnd4")
-        let hotelObject5 = new Hotel("name5", "https://i.picsum.photos/id/816/300/200.jpg?hmac=LyDUxx5Jqqh7V5H3YZdOFIsgVEmG9IWw4H8Iwjfs46I")
-        let hotelObject6 = new Hotel("name6", "https://i.picsum.photos/id/721/300/200.jpg?hmac=a8J-TueLrN5E8F50mFD-1efhdFssR3Mj-FuuUHRpddY")
-        let hotelObject7 = new Hotel("name7", "https://i.picsum.photos/id/998/300/200.jpg?hmac=ABcJlM1FNcyP3AfTFRqfa64HxavGYS_EloO3wVKI6WY")
-        let hotelObject8 = new Hotel("name8", "https://i.picsum.photos/id/173/300/200.jpg?hmac=QNv1dkRQYgkul__n_maFKcC5QLE7zBly8z3gwr3SmI0")
+        let hotelObject01 = new Hotel("name01", "https://i.picsum.photos/id/645/300/200.jpg?hmac=zSwhK0ipngA3Q8gPxiRsL3NGUmKtCy89EcH8zONvh8M")
+        let hotelObject02 = new Hotel("name02", "https://i.picsum.photos/id/78/300/200.jpg?hmac=n7Sj8aECCS5WoJkWO-zflsPVDeezs5U4M7WgQjLACA0")
+        let hotelObject03 = new Hotel("name03", "https://i.picsum.photos/id/641/300/200.jpg?hmac=YpOnhDuvo6GeXZPS8yR6Wf62YHrXGlWTDObszZf3zpI")
+        let hotelObject04 = new Hotel("name04", "https://i.picsum.photos/id/331/300/200.jpg?hmac=uIUrcfY5rEjbMirOCMEvEXzC0tIJzFIBfZBZ4qhvnd4")
+        let hotelObject05 = new Hotel("name05", "https://i.picsum.photos/id/816/300/200.jpg?hmac=LyDUxx5Jqqh7V5H3YZdOFIsgVEmG9IWw4H8Iwjfs46I")
+        let hotelObject06 = new Hotel("name06", "https://i.picsum.photos/id/721/300/200.jpg?hmac=a8J-TueLrN5E8F50mFD-1efhdFssR3Mj-FuuUHRpddY")
+        let hotelObject07 = new Hotel("name07", "https://i.picsum.photos/id/998/300/200.jpg?hmac=ABcJlM1FNcyP3AfTFRqfa64HxavGYS_EloO3wVKI6WY")
+        let hotelObject08 = new Hotel("name08", "https://i.picsum.photos/id/173/300/200.jpg?hmac=QNv1dkRQYgkul__n_maFKcC5QLE7zBly8z3gwr3SmI0")
+        let hotelObject09 = new Hotel("name09", "https://i.picsum.photos/id/203/300/200.jpg?hmac=FsCLTwngZ8-1jbxqdBKyoJy-ZHjbhlupNzeKiib04MI")
+        let hotelObject10 = new Hotel("name10", "https://i.picsum.photos/id/718/300/200.jpg?hmac=2AXNXV_A7Vmp-rOv8sWOWmRkGUZUJmc8pP3Gvsmu-PE")
 
-        let addRequest = hotelObjectStore.add(hotelObject1)
-        addRequest = hotelObjectStore.add(hotelObject2)
-        addRequest = hotelObjectStore.add(hotelObject3)
-        addRequest = hotelObjectStore.add(hotelObject4)
-        addRequest = hotelObjectStore.add(hotelObject5)
-        addRequest = hotelObjectStore.add(hotelObject6)
-        addRequest = hotelObjectStore.add(hotelObject7)
-        addRequest = hotelObjectStore.add(hotelObject8)
+        let addRequest = hotelObjectStore.add(hotelObject01)
+        addRequest = hotelObjectStore.add(hotelObject02)
+        addRequest = hotelObjectStore.add(hotelObject03)
+        addRequest = hotelObjectStore.add(hotelObject04)
+        addRequest = hotelObjectStore.add(hotelObject05)
+        addRequest = hotelObjectStore.add(hotelObject06)
+        addRequest = hotelObjectStore.add(hotelObject07)
+        addRequest = hotelObjectStore.add(hotelObject08)
+        addRequest = hotelObjectStore.add(hotelObject09)
+        addRequest = hotelObjectStore.add(hotelObject10)
+
+        for (let i = 11; i < 61; i++) {
+            let hotelObject = new Hotel("name" + i, "https://i.picsum.photos/id/718/300/200.jpg?hmac=2AXNXV_A7Vmp-rOv8sWOWmRkGUZUJmc8pP3Gvsmu-PE")
+            addRequest = hotelObjectStore.add(hotelObject)
+        }
 
         addRequest.onerror = function () {
             console.log("Add Request Error", addRequest.error)
@@ -64,18 +79,6 @@ function databaseInit() {
 
     openRequest.onerror = function () {
         console.error("Open Request Error", openRequest.error)
-    }
-}
-
-function addMultiple(database, datas, callback) {
-    const transaction = database.transaction(["people"], "readwrite")
-
-    datas.forEach(data => {
-        let request = transaction.objectStore("people").add(data)
-    })
-
-    transaction.oncomplete = function(event) {
-        callback()
     }
 }
 
@@ -97,22 +100,13 @@ function initialize() {
     filterCancelButton.addEventListener("click", cancelFilters)
     filterApplyButton.addEventListener("click", applyFilters)
 
-    window.onscroll = function () {
-        scrolledTooFarDownCheck(10000)
-    }
+    previousPage.addEventListener("click", goToPreviousPage)
+    nextPage.addEventListener("click", goToNextPage)
 }
 
 function scrollToTop() {
     document.body.scrollTop = 0
     document.documentElement.scrollTop = 0
-}
-
-function scrolledTooFarDownCheck(limit) {
-    if (document.body.scrollTop > limit || document.documentElement.scrollTop > limit) {
-        html.style.scrollBehavior = "auto"
-    } else {
-        html.style.scrollBehavior = "smooth"
-    }
 }
 
 function openSidebar(width) {
@@ -158,8 +152,22 @@ function applyFilters() {
 
 function showHotelList() {
     getHotels().then(function (hotels) {
-        for (let i = 0; i < hotels.length; i++) {
-            makeHotelDiv(hotels[i].name, hotels[i].imageURL)
+        const numberOfPages = (hotels.length % hotelNumberOnEachPage === 0) ?
+            (hotels.length / hotelNumberOnEachPage) :
+            (Math.floor(hotels.length / hotelNumberOnEachPage) + 1)
+        if (activePageNum < 1) {
+            activePageNum = 1
+        } else if (activePageNum > numberOfPages) {
+            activePageNum = numberOfPages
+        } else {
+            hotelListArea.textContent = ""
+            const endBound = (hotelNumberOnEachPage * activePageNum < hotels.length) ?
+                (hotelNumberOnEachPage * activePageNum) :
+                hotels.length
+            for (let i = hotelNumberOnEachPage * (activePageNum - 1); i < endBound; i++) {
+                makeHotelDiv(hotels[i])
+            }
+            scrollToTop()
         }
     })
 }
@@ -188,16 +196,30 @@ function getHotels() {
     })
 }
 
-function makeHotelDiv(name, imageURL) {
+function makeHotelDiv(hotelObj) {
     const wrapper = document.createElement("div")
+    wrapper.style.backgroundColor = "orange"
 
     const hotelNameElement = document.createElement("h1")
-    hotelNameElement.appendChild(document.createTextNode(name))
+    hotelNameElement.style.textAlign = "center"
+    hotelNameElement.style.userSelect = "none"
+    hotelNameElement.appendChild(document.createTextNode(hotelObj.name))
     wrapper.appendChild(hotelNameElement)
 
     const image = document.createElement("img")
-    image.setAttribute("src", imageURL)
+    image.setAttribute("src", hotelObj.imageURL)
+    image.style.margin = "0 20px 25px"
     wrapper.appendChild(image)
 
     hotelListArea.appendChild(wrapper)
+}
+
+function goToPreviousPage() {
+    activePageNum--
+    showHotelList()
+}
+
+function goToNextPage() {
+    activePageNum++
+    showHotelList()
 }
